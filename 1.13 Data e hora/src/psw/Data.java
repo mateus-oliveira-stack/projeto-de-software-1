f8282911e550e208584602bc6dcd1bcc107a818b
Mateus de Oliveira Rippler <mateus.oliveira.cc@hotmail.com>
2018-09-23 20:33:10 -0300

    Validação de data incluída

--- 1.13 Data e hora/src/psw/Data.java
+++ 1.13 Data e hora/src/psw/Data.java
@@ -4,11 +4,28 @@ public class Data {
 	private int dia, mes, ano;
 	
 	public void inicializaData(int dia, int mes, int ano) {
-		this.dia = dia;
-		this.mes = mes;
-		this.ano = ano;
+		if(dia < 1 || dia > 31 || mes < 1 || mes > 12|| ano < 1) {
+			this.dia = 0;
+			this.mes = 0;
+			this.ano = 0;
+		}
+		else {
+			this.dia = dia;
+			this.mes = mes;
+			this.ano = ano;
+		}
 	}
-
+	
+	public boolean dataEValida() {
+		if(this.dia > 31 || this.dia < 1)
+			return false;
+		if(this.mes < 1 || this.mes > 12)
+			return false;
+		if(this.ano < 1)
+			return false;
+		
+		return true;
+	}
 	public int getDia() {
 		return dia;
 	}
@@ -34,6 +51,9 @@ public class Data {
 	}
 	
 	public String mostraData() {
+		if(this.dia == 0 || this.mes == 0 || this.ano == 0)
+			return String.format("Data inválida");
+		
 		return String.format("Data: %02d/%02d/%02d", this.dia, this.mes, this.ano);
 	}
 }
